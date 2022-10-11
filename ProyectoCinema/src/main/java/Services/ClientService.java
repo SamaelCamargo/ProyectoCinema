313c7c778b2ac7e3f58ab7291ef03e23346130e7
SamaelCamargo <112991417+SamaelCamargo@users.noreply.github.com>
2022-10-10 21:02:47 -0500

    Reto 4 Client controller

--- ProyectoCinema/src/main/java/Services/ClientService.java
+++ ProyectoCinema/src/main/java/Services/ClientService.java
@@ -3,10 +3,12 @@ package Services;
 import Repository.ClientRepository;
 import model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
