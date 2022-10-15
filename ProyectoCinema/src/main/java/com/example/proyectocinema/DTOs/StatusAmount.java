0580302052a90877ba4c3154fd02ebf48a5c987c
LuisaHerreraCh <luisa.herrera.mt@usa.edu.co>
2022-10-14 23:59:24 -0500

    Se Ajusto para el reto 5 la clase CountClient del paquete DTOs

--- ProyectoCinema/src/main/java/com/example/proyectocinema/DTOs/StatusAmount.java
+++ ProyectoCinema/src/main/java/com/example/proyectocinema/DTOs/StatusAmount.java
@@ -1,10 +1,12 @@
-package com.example.proyectocinema.Personalizado;
+package com.example.proyectocinema.DTOs;
 
 public class StatusAmount {
     private int completed;
     private int cancelled;
 
     public StatusAmount(int size, int size1) {
+        this.completed=size;
+        this.cancelled=size1;
     }
 
     public int getCompleted() {
