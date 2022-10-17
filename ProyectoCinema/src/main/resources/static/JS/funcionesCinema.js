2f713ca3aa3d74a9bbe1d2075ee145f5cddb30d2
LuisaHerreraCh <luisa.herrera.mt@usa.edu.co>
2022-10-17 18:05:18 -0500

    Revert "se realiza el Front de Mensajes y Cinemas."
    This reverts commit 8b3175135b5f501437d8f96339532adb47ccf6c2.

--- ProyectoCinema/src/main/resources/static/JS/funcionesCinema.js
+++ ProyectoCinema/src/main/resources/static/JS/funcionesCinema.js
@@ -3,103 +3,36 @@ const url = "http://localhost:8081/api";
 class Cinema {
     static insert() {
         //Cuerpo de envio
-        if ($("#idCinema").val()==""|| $("#idPropietario").val()== "" || $("#idCapacity").val() == "" || $("#idDescripcion").val()== "") {
-            alert("Todos los campos son obligatorios para crear el Cinema");
-        } else {
-
-            const cinema = {
-                name: $("#idCinema").val(),
-                owner: $("#idPropietario").val(),
-                capacity: $("#idCapacity").val(),
-                category: {idCategoriy: +$("#select-categoria").val()},
-                Description: $("#idDescripcion").val()
-            }
-
-            $.ajax({
-                url: url + "/Cinema/save",
-                type: "POST",
-                dataType: "json",
-                crossDomain: true,
-                data: JSON.stringify(cinema),
-                contentType: "application/json",
-                complete: function (response) {
-                    if (response.status === 201) {
-                        $("#idCinema").val(""),
-                            $("#idPropietario").val(""),
-                            $("#idCapacity").val(""),
-                            $("#select-categoria").val(""),
-                            $("#idDescripcion").val(""),
-                            Cinema.loadAll();
-                        alert("Cinema agregado");
-                    } else {
-                        alert("se produjo un error,el Cinema pudo ser creado");
-                    }
-                }
-            });
+        const cinema = {
+            id: $("#idCinema").val(),
+            owner: $("#propietarioCinema").val(),
+            capacity: $("#capacidadCinema").val(),
+            category_ID: $("#categoriaCinema").val(),
+            name: $("#nombreCinema").val()
         }
-    }
 
-    static update(botonActualizar) {
-        if ($("#idCinema").val() == "" || $("#idPropietario").val()=="" || $("#idCapacity").val()=="" || $("#idDescripcion").val()=="") {
-            alert("Todos los campos son obligatorios para actualizar el Cinema");
-        } else {
-
-
-            let camposCinema = {
-                idCinema:botonActualizar,
-                name: $("#idCinema").val(),
-                owner: $("#idPropietario").val(),
-                capacity: $("#idCapacity").val(),
-                category: {idCategoriy: +$("#select-categoria").val()},
-                Description: $("#idDescripcion").val()
-            }
-
-            $.ajax({
-                url: url + "/Cinema/update",
-                type: "PUT",
-                dataType: "json",
-                crossDomain: true,
-                data: JSON.stringify(camposCinema),
-                contentType: "application/json",
-                complete: function (response) {
-                    if (response.status === 201) {
-                        $("#idCinema").val(""),
-                            $("#idPropietario").val(""),
-                            $("#idCapacity").val(""),
-                            $("#select-categoria").val(""),
-                            $("#idDescripcion").val(""),
-                            Cinema.loadAll();
-
-                        alert("Cinema actualizado");
-                    } else {
-                        alert("El Cinema no se pudo actualizar");
-                    }
-                }
-            });
-
-        }
-    }
-    static deleteById(idBoton) {
-        let myData={
-            id:idBoton
-        };
         $.ajax({
-            url: ApiURLCinema+"/Cinema/delete/"+idBoton,
-            type: "DELETE",
+            url: url + "/Cinema/save",
+            type: "POST",
             dataType: "json",
             crossDomain: true,
-            data: JSON.stringify(myData),
+            data: JSON.stringify(cinema),
             contentType: "application/json",
             complete: function (response) {
-                if (response.status === 204) {
+                if (response.status === 201) {
+                    $("#idCinema").val(""),
+                        $("#propietarioCinema").val(""),
+                        $("#capacidadCinema").val(""),
+                        $("#categoriaCinema").val(""),
+                        $("#nombreCinema").val(""),
+                        $("#detallesCinema").html("<p>Seleccione un cinema</p>")
                     Cinema.loadAll();
-                    alert("Cinema eliminado");
+                    alert("Cinema agregado");
                 } else {
-                    alert("se produjo un eroor, el Cinema no pudo ser eliminado");
+                    alert("Cinema no agregado");
                 }
             }
         });
-
     }
 
     static loadAll() {
@@ -115,18 +48,16 @@ class Cinema {
                 for (let index = 0; index < data.items.length; index++) {
                     $("tbody").append(`
                     <tr>
-                        <td>${data.items[index].name}</td>
+                        <td>${data.items[index].id}</td>
                         <td>${data.items[index].owner}</td>                        
                         <td>${data.items[index].capacity}</td>
-                        <td>${data.items[index].category.name}</td>
-                        <td>${data.items[index].Description}</td>
-                       <td> <button onclick='Cinema.update ("+items[i].id+")'> Actualizar </button> " ;
-                       <td> <button onclick='Cinema.deleteById ("+items[i].id+")'> Borrar </button> " ;
-                       
+                        <td>${data.items[index].category_ID}</td>
+                        <td>
+                            <a href = "javascript:Cinema.loadById(${data.items[index].id})" class="link-success">${data.items[index].name}</a>
+                        </td>
                     </tr>
                     `);
                 }
-                $("#resultado2").html(myTable);
             },
             error: function () {
                 alert("se produjo un error, los cinemas no fueron cargados")
@@ -149,7 +80,7 @@ class Cinema {
                     <p><b>Id:</b> ${(data.items[0].id)} </p>
                     <p><b>Propietario:</b> ${data.items[0].owner}</p>
                     <p><b>Capacidad:</b> ${data.items[0].capacity}</p>
-                    <p><b>Categoria:</b>  ${data.items[0].category}</p>
+                    <p><b>Categoria:</b>  ${data.items[0].category_ID}</p>
                     <p><b>Nombre:</b>  ${data.items[0].name}</p>
                     <button onclick="Cinema.deleteById( ${(data.items[0].id)})"type="button" id="EliminarCinema" class="btn btn-success">ELIMINAR</button>
 
@@ -161,22 +92,57 @@ class Cinema {
             }
         });
     }
-    static getCategoriaRelacion() {
+
+    static update() {
+        const cinema = {
+            id: $("#idCinema").val(),
+            owner: $("#propietarioCinema").val(),
+            capacity: $("#capacidadCinema").val(),
+            category_ID: $("#categoriaCinema").val(),
+            name: $("#nombreCinema").val()
+        }
+
         $.ajax({
-            url: url + "/Category/all",
-            type: "GET",
+            url: ApiURLCinema,
+            type: "PUT",
+            dataType: "json",
             crossDomain: true,
-            datatype: "JSON",
-            success: function (respuesta) {
-                let $select = $("#select-categoria");
-                $.each(respuesta, function (id, name) {
-                    $select.append('<option value=' + name.idCategoriy + '>' + name.name + '</option>');
-                    //console.log(name);
-                });
+            data: JSON.stringify(cinema),
+            contentType: "application/json",
+            complete: function (response) {
+                if (response.status === 201) {
+                    $("#idCinema").val(""),
+                        $("#propietarioCinema").val(""),
+                        $("#capacidadCinema").val(""),
+                        $("#categoriaCinema").val(""),
+                        $("#nombreCinema").val(""),
+                        $("#detallesCinema").html("<p>Seleccione un cinema</p>")
+                    Cinema.loadAll();
+                    alert("Cinema actualizado");
+                } else {
+                    alert("Cinema no se pudo actualizar");
+                }
             }
-
         });
-
     }
 
+    static deleteById(id) {
+        $.ajax({
+            url: ApiURLCinema,
+            type: "DELETE",
+            dataType: "json",
+            crossDomain: true,
+            data: JSON.stringify({id}),
+            contentType: "application/json",
+            complete: function (response) {
+                if (response.status === 204) {
+                    $("#detallesCinema").html("<p>Seleccione un cliente</p>")
+                    Cinema.loadAll();
+                    alert("Cinema eliminado");
+                } else {
+                    alert("Cinema no sea a eliminado");
+                }
+            }
+        });
+    }
 }
